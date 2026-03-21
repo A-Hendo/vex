918cd85c423ff7d67b13211ef893727104552874
Adam Henderson <adamhenderson90@gmail.com>
2026-03-21 21:27:22 +0000

    tests: improve type safety and formatting in test_main

--- vex/__init__.py
+++ vex/__init__.py
@@ -1,5 +1,5 @@
-"""This module makes "vex" a package.
-"""
-from . _version import VERSION as __version__
+"""This module makes "vex" a package."""
+
+from ._version import VERSION as __version__
 
 __all__ = ["__version__"]
--- vex/tests/test_main.py
+++ vex/tests/test_main.py
@@ -9,6 +9,7 @@ from typing import Any
 import pytest
 
 from vex import main
+from vex.options import get_options
 from vex.config import Vexrc
 from vex import exceptions
 from vex.tests.fakes import Object, make_fake_exists
@@ -16,28 +17,28 @@ from vex.tests.fakes import Object, make_fake_exists
 
 class TestGetVexrc:
     def test_get_vexrc_nonexistent(self) -> None:
-        options = main.get_options(["--config", "unlikely_to_exist"])
+        options = get_options(["--config", "unlikely_to_exist"])
         with pytest.raises(exceptions.InvalidVexrc) as excinfo:
             main.get_vexrc(options, {})
         assert "unlikely_to_exist" in str(excinfo.value)
 
     def test_get_vexrc(self) -> None:
-        options = main.get_options(["--config", "pretends_to_exist"])
+        options = get_options(["--config", "pretends_to_exist"])
 
         def fake_open(name: str, mode: str) -> BytesIO:
             assert name == "pretends_to_exist"
             assert mode == "rb"
             return BytesIO(b"a=b\n")
 
-        with patch("os.path.exists", return_value=True), patch(
-            "builtins.open", create=True, side_effect=fake_open
+        with (
+            patch("os.path.exists", return_value=True),
+            patch("builtins.open", create=True, side_effect=fake_open),
         ):
             vexrc = main.get_vexrc(options, {})
             assert vexrc
 
 
 class TestGetCwd:
-
     def test_get_cwd_no_option(self) -> None:
         options = argparse.Namespace(cwd=None)
         assert main.get_cwd(options) is None
@@ -56,7 +57,6 @@ class TestGetCwd:
 
 
 class TestGetVirtualenvPath:
-
     def test_no_ve_base(self) -> None:
         with pytest.raises(exceptions.NoVirtualenvsDirectory):
             main.get_virtualenv_path("", "anything")
@@ -68,16 +68,18 @@ class TestGetVirtualenvPath:
     def test_no_ve_name(self) -> None:
         fake_path = os.path.abspath("pretends_to_exist")
         fake_exists = make_fake_exists([fake_path])
-        with patch("os.path.exists", side_effect=fake_exists), pytest.raises(
-            exceptions.InvalidVirtualenv
+        with (
+            patch("os.path.exists", side_effect=fake_exists),
+            pytest.raises(exceptions.InvalidVirtualenv),
         ):
             main.get_virtualenv_path(fake_path, "")
 
     def test_nonexistent_ve_path(self) -> None:
         fake_path = os.path.abspath("pretends_to_exist")
         fake_exists = make_fake_exists([fake_path])
-        with patch("os.path.exists", side_effect=fake_exists), pytest.raises(
-            exceptions.InvalidVirtualenv
+        with (
+            patch("os.path.exists", side_effect=fake_exists),
+            pytest.raises(exceptions.InvalidVirtualenv),
         ):
             main.get_virtualenv_path(fake_path, "/unlikely_to_exist2")
 
@@ -92,7 +94,6 @@ class TestGetVirtualenvPath:
 
 
 class TestGetCommand:
-
     def test_shell_options(self) -> None:
         vexrc = Vexrc()
         vexrc[vexrc.default_heading]["shell"] = "/bin/dish"  # type: ignore
